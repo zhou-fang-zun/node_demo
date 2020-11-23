3d009b7326adb9d975aa8b056eade728f85e877c
zhou-fang-zun <1215236758@qq.com>
2020-11-24 00:32:06 +0800

    登录

--- router/admin.js
+++ router/admin.js
@@ -16,27 +16,59 @@ router.post('/register',(req,res) => {
 			if(!name) throw new Error('用户名错误')
 			if(!pwd) throw new Error('密码错误')
 			if(!phone) throw new Error('手机号错误')
-		}catch(err => {
+		}catch(err){
 			res.send({ status: 0, success: false, msg: err.message })
 			return
-		})
+		}
+		const newpassword = md5(pwd)
 		try{
 			const admin = User.findOne({name})
 			if(admin){
 				res.send({ status: 0, success: false, msg: '该用户已经存在' })
 			}else{
 				const admin_id = await getId('admin_id')
+				const postData = {
+					name, pwd: newpassword, gender, phone, avatar, id: admin_id
+				}
+				await User.create(postData)
+				req.session.admin_id = admin_id
+				res.send({ status: 1, success: true, msg: '注册成功' })
 			}
-		}catch(err => {
+		}catch(err){
 			res.send({ status: 0, success: false, msg: '注册失败' })
 			return
-		})
+		}
 	})
-
-	let post = { name, pwd, repwd, gender, phone, avatar }
-	
 })
 
+//登录
+router.post('/loginin',(req,res) =>{
+	const cap = req.cookies.cap
+	if(!cap){
+		res.send({ status: 0, success: false, msg: '验证码失效' })
+		return
+	}
+	const form = new formidable.IncomingForm()
+	form.parse(req,async (err,fields,files) => {
+		if(err){
+			res.send({ status: 0, success: false, msg: '表单信息错误'})
+			return
+		}
+		const { name, pwd, captcha } = fields
+		try{
+			if(!name) throw new Error('用户名参数错误')
+			if(!pwd) throw new Error('密码参数错误')
+			if(!captcha) throw new Error('验证码参数错误')
+		}catch(err){
+			res.send({ status: 0, success: false, msg: err.message})
+			return
+		}
+		if(cap.toString() !== captcha.toString()){
+			res.send({ status: 0, success: false, msg: '验证码不正确'})
+			return
+		}
+	})
+})
 //req.session.user = user
 //find  查找
 //insertMany  插入
