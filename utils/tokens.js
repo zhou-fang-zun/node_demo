5486d948399607470e643f12b31bd6aacad0c71d
zhou-fang-zun <1215236758@qq.com>
2020-11-26 00:39:24 +0800

    utils

---
+++ utils/tokens.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken')
+
+class Tokens{
+    static async VerifyToken(req,res,next){
+        let reqBody = req.body
+        let token = reqBody.accessToken || req.query.accessToken || req.headers['access-token'] || req.cookies.accessToken
+        if(token){
+            //存在token解析token
+            await jwt.verify(token,'client',(err,decoded) => {
+                if(err){
+                    res.send({ status: 0, success: false, msg: '当前用户未登录，请重新登录', data:{
+                        islogin: false,
+                        userInfo: {}
+                    }})
+                }else{
+                    let userInfo = await UserModel.findOne({
+                        where: {uid: decoded.uid},
+                    })
+                    if(userInfo){
+                        req.islogin = true
+                        req.userInfo = userInfo
+                    }else{
+                        req.islogin = false
+                        req.userInfo = {}
+                    }
+                    next()
+                }
+            })
+        }else{
+            res.send({ status: 0, success: false, msg: '当前用户未登录，请重新登录', data:{
+                islogin: false,
+                userInfo: {}
+            }})
+        }
+    }
+
+    static setToken(time,data){
+        return jwt.sign(data,'client',{
+            expirseIn: time
+        })
+    }
+}
+
+module.exports = Tokens
